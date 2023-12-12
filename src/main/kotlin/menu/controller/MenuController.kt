9fee786a83e3158925a6a2094715d850ae27366a
stopstone <wltjr6432@naver.com>
2023-12-13 00:54:41 +0900

    feat: 프로그램 실행을 위한 controller 추가

---
+++ src/main/kotlin/menu/controller/MenuController.kt
@@ -0,0 +1,59 @@
+package menu.controller
+
+import menu.domain.Coach
+import menu.domain.MenuRecommend
+import menu.validator.ValidateCoach.coachValidate
+import menu.view.InputView
+import menu.view.OutputView
+
+class MenuController {
+    private val outputView = OutputView()
+    private val inputView = InputView()
+    private val menuRecommend = MenuRecommend()
+
+    private var coaches = mutableListOf<Coach>()
+
+    init {
+        outputView.printServiceStartMessage()
+    }
+
+    fun programRun() {
+        val coachNames = readCoachNames()
+        val cantEatMenus = readCantEatMenus(coachNames)
+        createCoachItems(coachNames, cantEatMenus)
+        outputView.printResultRecommend()
+        outputView.printWeekday()
+        outputView.printCategory(menuRecommend.getWeekdayCategory())
+        outputView.printRecommendMenu(coaches)
+        outputView.printServiceEndMessage()
+    }
+
+
+    private fun readCoachNames(): List<String> {
+        outputView.readCoachNamesMessage()
+        while (true) {
+            try {
+                val coachNames = inputView.readCoachNames()
+                coachValidate(coachNames)
+                return coachNames
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun readCantEatMenus(coachNames: List<String>): MutableList<List<String>> {
+        val cantEatMenus = mutableListOf<List<String>>()
+        coachNames.forEach { name ->
+            outputView.readCantEatMenusMessage(name)
+            cantEatMenus.add(inputView.readCantEatMenus())
+        }
+        return cantEatMenus
+    }
+
+    private fun createCoachItems(coachNames: List<String>, cantEatMenus: MutableList<List<String>>) {
+        for (idx in coachNames.indices) {
+            coaches.add(Coach(coachNames[idx], cantEatMenus[idx], menuRecommend))
+        }
+    }
+}
